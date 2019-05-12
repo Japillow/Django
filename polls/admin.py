06f38f13dc026053273aa24fb7d3b73380f235eb
Japillow <james@barrios.me>
2019-05-12 20:52:51 +0100

    Adding related objects in admin form

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -9,9 +14,11 @@ class QuestionAdmin(admin.ModelAdmin):
             'fields': ['question_text']
         }),
         ('Date information', {
-            'fields': ['pub_date']
+            'fields': ['pub_date'],
+            'classes': ['collapse']
         }),
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
